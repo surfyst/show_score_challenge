a6287b9b8d19309c0e687e682d658f10c7a4b4ed
Wyss Star <surfyst@gmail.com>
2016-02-06 21:45:23 -0500

    Order by random + avg score

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,10 +1,16 @@
 class ShowsController < ApplicationController
   def index
-    sql = %q(
-      SELECT shows.title, avg(reviews.score) avg_score, count(reviews.show_id) review_count FROM shows
-      INNER JOIN reviews ON reviews.show_id = shows.id GROUP BY reviews.show_id
-      ORDER BY avg_score DESC LIMIT 10
-    )
+    seed = Time.now.min / 60.0
+    sql = "
+      SELECT setseed(#{seed}), shows.title, avg(reviews.score) * random() avg_score, count(reviews.show_id) review_count FROM shows
+      INNER JOIN reviews ON reviews.show_id = shows.id
+      GROUP BY shows.title, reviews.show_id
+      ORDER BY avg_score DESC LIMIT 10"
     @shows = Show.find_by_sql(sql)
+
+    respond_to do |format|
+      format.json { render json: @shows}
+      format.html
+    end
   end
 end
